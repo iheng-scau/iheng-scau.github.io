58e788512c9f125770490411a3ac597957297a27
iheng-scau <iheng_scau@hotmail.com>
2015-09-19 23:25:27 +0800

    20150919-3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $(".main-content").load("pages/main.html");
+
     $("#nav-ul li").click(function () {
         var item_id = $(this).attr("id");
         var location = "../pages/" + item_id + ".html";
@@ -6,6 +8,11 @@ $(document).ready(function () {
     });
 
     $("#site-tips").animate({ top: '0px' });
+
+    $(window).resize(function () {
+        var nav_width = $(".nav").width();
+        var main_width = $(".main").width();
+    });
 });
 
 function action_closeTip() {
