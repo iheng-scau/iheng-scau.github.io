b901d26426013bfeeef9b758c88328dfb61efcbf
iheng <iheng_scau@hotmail.com>
2015-09-22 14:40:35 +0800

    添加侧边栏简繁模式切换，但是未考虑宽度调整，代码注释暂时不投入使用

--- js/index.js
+++ js/index.js
@@ -16,8 +16,29 @@ $(document).ready(function () {
         var nav_height = $(".nav").height();
         $(".main").css("height", nav_height);
     });
+
+    //将nav切换为简约模式
+    /**
+    $("#resize-nav").click(function () {
+        var class_name = $("#nav").attr("class");
+        if (class_name == "nav") {
+            $("#nav").animate({width:'50px'});
+            $("#nav").removeClass("nav").addClass("nav-narrow");
+            return;
+        }
+        $("#nav").removeClass("nav-narrow").addClass("nav");
+    });**/
 });
 
 function action_closeTip() {
     $("#site-tips").animate({ top: '-30px' });
+}
+
+function action_shrinkFrontHeader() {
+    var main_header_height = $(".main-header").css("height");
+    if (main_header_height == "150px") {
+        $(".main-header").animate({ height: '65px' });
+        return;
+    }
+    $(".main-header").animate({ height: '150px' });
 }
