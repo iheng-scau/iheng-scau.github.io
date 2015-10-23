6b68ed1d2e68ee1da9222633b06607a4a6bac255
iheng <iheng_scau@hotmail.com>
2015-10-23 15:13:18 +0800

    20151023

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $(".timeline li:gt(0) .content-desc").hide();
 
     $(".hide").click(function () {
+        $(this).toggleClass("rotate");
         $(this).parents(".timestamp-content").children(".content-desc").toggle("normal");
     });
 });
