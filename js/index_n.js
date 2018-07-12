cf3afcfd038fb937ad15afb4ac95b9ff8014475b
iheng <zhangdh@jpush.cn>
2018-07-13 00:03:11 +0800

    remove github fork badge

--- js/index_n.js
+++ js/index_n.js
@@ -3,11 +3,17 @@ $(function () {
     $("#article-content").load("pages/articles.html");
     $("#profile-content").load("pages/profiles.html");
 
-    $(".nav li:eq(1)").css("width","70%");
-    $(".nav li:eq(1)").siblings("li").animate({"width":"15%","opacity":"0.2"},"fast");
+    $(".nav li:eq(1)").css("width", "70%");
+    $(".nav li:eq(1)").siblings("li").animate({ "width": "15%", "opacity": "0.2" }, "fast");
 
-    $(".nav li").click(function(){
-        $(this).siblings("li").animate({"width":"15%","opacity":"0.2"},"fast");
-        $(this).animate({"width":"70%","opacity":"1"});
+    var curli = 1;
+    $(".nav li").click(function () {
+        var clickli = $(this).index();
+        if (curli == clickli) {
+            return;
+        }
+        curli = clickli;
+        $(this).siblings("li").animate({ "width": "15%", "opacity": "0.2" }, "fast");
+        $(this).animate({ "width": "70%", "opacity": "1" });
     });
 });
