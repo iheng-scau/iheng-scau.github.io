bc2b27270d98f8982a43a104a9c82f1be9de68fa
iheng <zhangdh@jpush.cn>
2018-07-15 00:34:59 +0800

    update

--- js/index_n.js
+++ js/index_n.js
@@ -2,6 +2,8 @@ $(function () {
     $("#project-content").load("pages/projects.html");
     $("#article-content").load("pages/articles.html");
     $("#profile-content").load("pages/profiles.html");
+    $("#favorite-content").load("pages/favorite.html");
+    $("#issues-content").load("pages/issues.html");
 
     $(".nav li:eq(2)").css("width", "40%");
     $(".nav li:eq(2)").siblings("li").animate({ "width": "15%", "opacity": "0.2" }, "fast");
