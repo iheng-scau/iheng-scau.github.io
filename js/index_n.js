f0082580a341d80c78ff764ab55fdbc1a10ee595
iheng <iheng_scau@hotmail.com>
2021-06-26 18:55:06 +0800

    新增主页内容

--- js/index_n.js
+++ js/index_n.js
@@ -2,18 +2,4 @@ $(function () {
     $("#articles-content").load("pages/articles.html");
     $("#events-content").load("pages/whatsup.html");
     $("#projects-content").load("pages/projects.html");
-
-    $(".nav li:eq(2)").css("width", "40%");
-    $(".nav li:eq(2)").siblings("li").animate({ "width": "20%", "opacity": "0.2" }, "fast");
-
-    var curli = 1;
-    $(".nav li").click(function () {
-        var clickli = $(this).index();
-        if (curli == clickli) {
-            return;
-        }
-        curli = clickli;
-        $(this).siblings("li").animate({ "width": "20%", "opacity": "0.2" }, "fast");
-        $(this).animate({ "width": "40%", "opacity": "1" });
-    });
-});
+}); 
