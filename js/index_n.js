6d1b303373e892610f173aa401d8d5c211a42a60
iheng <iheng_scau@hotmail.com>
2021-06-30 00:37:48 +0800

    页面更新

--- js/index_n.js
+++ js/index_n.js
@@ -2,4 +2,5 @@ $(function () {
     $("#articles-content").load("pages/articles.html");
     $("#events-content").load("pages/whatsup.html");
     $("#projects-content").load("pages/projects.html");
+    $("#game-content").load("pages/favorite.html");
 }); 
--- js/projects.js
+++ js/projects.js
@@ -1,15 +1,15 @@
 $(function () {
-    $(".nav li:eq(2)").css("width", "40%");
-    $(".nav li:eq(2)").siblings("li").animate({ "width": "20%", "opacity": "0.2" }, "fast");
+    $(".nav .tab:eq(0)").css("width", "40%");
+    $(".nav .tab:eq(0)").siblings("li").animate({ "width": "20%", "opacity": "0.2" }, "fast");
 
     var curli = 1;
-    $(".nav li").click(function () {
+    $(".nav .tab").click(function () {
         var clickli = $(this).index();
         if (curli == clickli) {
             return;
         }
         curli = clickli;
-        $(this).siblings("li").animate({ "width": "20%", "opacity": "0.2" }, "fast");
+        $(this).siblings(".tab").animate({ "width": "20%", "opacity": "0.2" }, "fast");
         $(this).animate({ "width": "40%", "opacity": "1" });
     });
 });
