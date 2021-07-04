01571eb3cfd79cd71d23d502e0e7f7a005b530c4
iheng <iheng_scau@hotmail.com>
2021-07-04 12:20:34 +0800

    update

--- js/projects.js
+++ js/projects.js
@@ -1,15 +1,3 @@
 $(function () {
-    $(".nav .tab:eq(0)").css("width", "40%");
-    $(".nav .tab:eq(0)").siblings("li").animate({ "width": "20%", "opacity": "0.2" }, "fast");
-
-    var curli = 1;
-    $(".nav .tab").click(function () {
-        var clickli = $(this).index();
-        if (curli == clickli) {
-            return;
-        }
-        curli = clickli;
-        $(this).siblings(".tab").animate({ "width": "20%", "opacity": "0.2" }, "fast");
-        $(this).animate({ "width": "40%", "opacity": "1" });
-    });
+    
 });
